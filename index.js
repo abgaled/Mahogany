761d327679bf8c90f84bcaf4e60cae445defc952
Abigale Del Rosario <delrosarioabigale@gmail.com>
2018-04-05 23:46:51 +0800

    nb

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 var express = require('express');
 var winston = require('winston');
 var app = express();
+var func = require('/app/index');
 
-var func = require('/app/index.js');
 
-
-func(app)
+func(app);
 
 app.listen(app.get('port'), () => {
     winston.info(`Mahogany Daycare Center is on PORT:${app.get('port')}`);
