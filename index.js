accbd08693d69f57c73bad00c8cdc5d6cdc1a0ea
Abigale Del Rosario <delrosarioabigale@gmail.com>
2018-04-05 22:55:59 +0800

    w

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 
 var func = require('./app/index.js');
 func(app);
-// require('./app')(app);
+
 
 app.listen(app.get('port'), () => {
     winston.info(`Mahogany Daycare Center is on PORT:${app.get('port')}`);
